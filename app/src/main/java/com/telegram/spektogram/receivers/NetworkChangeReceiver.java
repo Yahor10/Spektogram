516fcda7c927ff0e20d9773cf87010dc53d1349c
Egor Chebotarev <T_EgorC1@verifone.com>
2015-05-06 11:54:58 +0300

    update contacts

---
+++ app/src/main/java/com/telegram/spektogram/receivers/NetworkChangeReceiver.java
@@ -0,0 +1,21 @@
+package com.telegram.spektogram.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.telegram.spektogram.preferences.PreferenceUtils;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+
+        final int connectivityStatus = NetworkUtil.getConnectivityStatus(context);
+        if (connectivityStatus == NetworkUtil.TYPE_NOT_CONNECTED) {
+            PreferenceUtils.setOfflineMode(context, true);
+        } else {
+            PreferenceUtils.setOfflineMode(context, false);
+        }
+    }
+}
